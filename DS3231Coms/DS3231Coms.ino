d86df23e32167688a3ae854637568a22327241ed
JaxonKJones <jaxonkadejones@gmail.com>
2023-03-01 22:56:19 -0700

    updated DS3231COMS

--- DS3231Coms/DS3231Coms.ino
+++ DS3231Coms/DS3231Coms.ino
@@ -8,13 +8,68 @@ void setup() {
   Wire.begin();
   rtc.begin();
   // uncomment the following line to set the time and date for the first time
-  rtc.adjust(DateTime(2022, 2, 25, 2, 49, 0));
-  // rtc.enableInterrupts(RTC_DS3231::Alarm1);
-  // rtc.clearAlarm(RTC_DS3231::Alarm1);
-  // rtc.writeSqwPinMode(RTC_DS3231::SquareWave1Hz);
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
+
+  char* events[] = {"1:10:05:U", "2:20:01:L", "3:23:00:U"};
+  Serial.print("Current DOW:H:M :");
+  Serial.print(rtc.now().dayOfTheWeek());
+  Serial.print(":");
+  Serial.print(rtc.now().hour());
+  Serial.print(":");
+  Serial.print(rtc.now().minute());
+  Serial.print(" Next Event:");
+  Serial.println(nextEvent(events, 3));
+  delay(5000);
+}
+
+String nextEvent(char* events[], int numEvents) {
+  DateTime now = rtc.now();      // get current date/time from RTC module
+  int currentDOW = now.dayOfTheWeek();
+  int currentHour = now.hour();
+  int currentMinute = now.minute();
+
+  int closestEventTime = 0;
+  String closestEvent = "";
+
+  for (int i = 0; i < numEvents; i++) {
+    int eventTime =0;
+    String event = String(events[i]);
+    int eventDOW = event.substring(0,1).toInt();
+    int eventHour = event.substring(2,4).toInt();
+    int eventMinute = event.substring(5,7).toInt();
+  
+    int timeToMidnight = (24 - currentHour) * 3600 + (60 - currentMinute) * 60; 
+
+    
+    if (eventDOW < currentDOW || (eventDOW == currentDOW && (eventHour < currentHour || (eventHour == currentHour && eventMinute <= currentMinute)))) {
+      // The event is in the past or next week
+      eventTime = timeToMidnight + (eventDOW - currentDOW) * 3600*24 + eventHour * 3600 + eventMinute * 60;
+      // Add one week to the time
+      eventTime += 3600*24*7;
+    }
+    else if(eventDOW > currentDOW){
+      // The event is in the future before the end of the week
+      eventTime = timeToMidnight + (eventDOW - currentDOW) * 3600*24 + eventHour * 3600 + eventMinute * 60;
+    }
+    else{
+      // The event is in the future today
+      eventTime = (eventHour - currentHour) * 3600 + (eventMinute - currentMinute) * 60;
+    }
+      
+
+    if (closestEventTime == 0 || eventTime < closestEventTime) {
+      closestEventTime = eventTime;
+      closestEvent = events[i];
+    }
+  }
+  return closestEvent;
+  delay(1000);
+}
+
+void getInput(){
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     if (command == "read") {
@@ -38,6 +93,41 @@ void loop() {
       Serial.println(" °C");
     }
   }
-
   delay(1000);
+}
+
+void DOW() {
+  DateTime now = rtc.now();      // get current date/time from RTC module
+  int dayOfWeek = now.dayOfTheWeek();  // get day of the week (0=Sunday, 1=Monday, etc.)
+  
+  // Print day of the week to serial monitor
+  Serial.print("Today is ");
+  switch (dayOfWeek) {
+    case 0:
+      Serial.println("Sunday");
+      break;
+    case 1:
+      Serial.println("Monday");
+      break;
+    case 2:
+      Serial.println("Tuesday");
+      break;
+    case 3:
+      Serial.println("Wednesday");
+      break;
+    case 4:
+      Serial.println("Thursday");
+      break;
+    case 5:
+      Serial.println("Friday");
+      break;
+    case 6:
+      Serial.println("Saturday");
+      break;
+    default:
+      Serial.println("Invalid day of the week");
+      break;
+  }
+
+  delay(1000);  // wait for 1 second before repeating loop
 }
--- smart_home_lock/smart_home_lock.ino
+++ smart_home_lock/smart_home_lock.ino
@@ -18,26 +18,32 @@ Servo lock;
 #define STATE_PIN 5  // set state pin to pin 4
 #define EN_PIN 4  // set enable pin to pin 5
 #define BUTTON_PIN 2  // set button pin to pin 2
-#define CLOCK_SQW 3 // interrupt pin from DS3231
+#define CLOCK 3 // interrupt pin from DS3231
 #define SERVO 9 // pwm pin for servo
 #define MOS 12 // servo enable pin
 SoftwareSerial BTserial(6, 7); // RX | TX
 
-
-int state = digitalRead(STATE_PIN);
-
 void setup() {
-  // Disable ADC
-  ADCSRA = 0;
-  // Set sleep mode to power-down
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  // Enable sleep mode
-  sleep_enable();
+  // Low Power Settings
+  ADCSRA = 0; // Disable ADC
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode to power-down
+  sleep_enable(); // Enable sleep mode
  
+  // Serial Settings
   Serial.begin(9600);
   BTserial.begin(9600);
+
+  //rtc config
   Wire.begin();
   rtc.begin();
+  pinMode(CLOCK, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(CLOCK), rtcInterrupt, FALLING);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+  rtc.disableAlarm(2);
+  rtc.setAlarm1(“00:00:01:00”, RTC_OFFSET, RTC_ALM1_MODE4 ); 
+
+
   lock.attach(9);
   pinMode(STATE_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT); // -- CHANGE TO BE INTERRUPT
@@ -45,7 +51,7 @@ void setup() {
   digitalWrite(EN_PIN, LOW);
   pinMode(MOS, OUTPUT);
   digitalWrite(MOS, LOW);
-  if(state == LOW){
+  if(digitalRead(STATE_PIN) == LOW){
     pair();
   }
 }
@@ -71,11 +77,8 @@ void loop() {
 }
 
 
-
-
-
 int menu(){
-  if(state == LOW){
+  if(digitalRead(STATE_PIN) == LOW){
     pair();
   }
   BTserial.write("\n---Main Menu---\n1: Scheduler\n2: Bluetooth Mode\n3: Schedule Mode\n4: Dev");
@@ -98,37 +101,25 @@ int menu(){
         return command;
       }
     }
+    if (digitalRead(STATE_PIN) == LOW){
+      pair();
+    }
   }
+    
 }
 
 void pair(){
   Serial.print("Pairing...");
-  int state = digitalRead(STATE_PIN);
-  while(state == LOW){
+  while(digitalRead(STATE_PIN) == LOW){
     Serial.print(" ... ");
     delay(3000);
-    state = digitalRead(STATE_PIN);
   } 
   Serial.println("Paired!");
 }
 
-void set_rtc(){
-  BTserial.write("--Set Time & Day (d:hh:mm)--");
-  while(true){
-    if (BTserial.available()) {
-      String time = BTserial.readString();
-      Serial.println(time);
-      int day = time.substring(0, 1).toInt();
-      int hour = time.substring(2, 4).toInt();
-      int minute = time.substring(5, 7).toInt();
-      break;
-    }
-  }
-}
-
 void scheduler(){  
   BTserial.write("\n--Scheduler--\n");
-  BTserial.write("Enter Schedule: (d:hh:mm:U/L) or exit\n");
+  BTserial.write("1:Add  2:Delete  3:See all Schedules   or Exit\n");
   // format is d:hh:mm - ex: 1:12:00 is Sunday at 12:00
   while(true){
     if (BTserial.available()) {
@@ -138,28 +129,55 @@ void scheduler(){
         Serial.println("Exiting...");
         break;
       }
-      else{
-        // Parse input to get hour, minute, and day of week
-        int day = input.substring(0, 1).toInt();
-        int hour = input.substring(2, 4).toInt();
-        int minute = input.substring(5, 7).toInt();
-        String stateTemp = input.substring(8, 9);
-        char state = stateTemp.charAt(0);
-        //save schedule to EEPROM
-        // save(day, hour, minute, state);
-        Serial.println(day);
-        Serial.println(hour);
-        Serial.println(minute);
-        Serial.println(state);
-        BTserial.write("\nSchedule set!");
+      else if (input == "1"){
+        //Add schedule
+        BTserial.write("\nEnter Schedule: (d:hh:mm:U/L) or Exit\n");
+        while(true){
+          if (BTserial.available()){
+            String input = BTserial.readString();
+            delay(30);
+            if (input == "0"){
+              Serial.println("Exiting...");
+              return;
+            }
+            else{
+              // Parse input to get hour, minute, and day of week
+              int day = input.substring(0, 1).toInt();
+              int hour = input.substring(2, 4).toInt();
+              int minute = input.substring(5, 7).toInt();
+              String stateTemp = input.substring(8, 9);
+              char state = stateTemp.charAt(0);
+              //save schedule to EEPROM
+              save(day, hour, minute, state);
+              // Serial.println(day);
+              // Serial.println(hour);
+              // Serial.println(minute);
+              // Serial.println(state);
+              BTserial.write("\nSchedule set!");
+            }
+          }
+        }
+
       }
+      else if(input == "2"){
+        //Delete schedule
+      }
+      else if(input == "3"){
+        //See all schedules
+        load(true);
+      }
+      
+    }
+    if(digitalRead(STATE_PIN) == LOW){
+      pair();
     }
   }
 }
 
 void save(int day, int hour, int minute, char state){
   //check eeprom to see where the next available slot is
-  int address = 3;
+  int address = 1;
+  int saves = EEPROM.read(0);
   while(EEPROM.read(address) != 0){
     address += 4;
   }
@@ -171,26 +189,46 @@ void save(int day, int hour, int minute, char state){
   EEPROM.write(address + 2, minute);
   //save state
   EEPROM.write(address + 3, state);
+  //save number of schedules
+  EEPROM.write(0, saves + 1);
 }
 
-void load(){
-  //load schedule from EEPROM
-  int address = 3;
-  while(EEPROM.read(address) != 0){
-    int day = EEPROM.read(address);
-    int hour = EEPROM.read(address + 1);
-    int minute = EEPROM.read(address + 2);
-    char state = EEPROM.read(address + 3);
-    // Serial.println(day);
-    // Serial.println(hour);
-    // Serial.println(minute);
-    // Serial.println(state);
-    address += 4;
+void load(bool print){
+  if (print){
+    Serial.print("Schedules: ");
+    Serial.println(EEPROM.read(0));
+    Serial.println("Day:  Hour:  Minute:  State:");
+    //print out all schedules from eeprom
+    int address = 1;
+    while(EEPROM.read(address) != 0){
+      int day = EEPROM.read(address);
+      int hour = EEPROM.read(address + 1);
+      int minute = EEPROM.read(address + 2);
+      char state = EEPROM.read(address + 3);
+      Serial.print(day);
+      Serial.print("      ");
+      Serial.print(hour);
+      Serial.print("      ");
+      Serial.print(minute);
+      Serial.print("      ");
+      Serial.println(state);
+      
+      address += 4;
+    }
   }
+  else{
+    //sort all schedules stored in eeprom
+
+    //compare current time to find the next closest scheduled event
+
+    //set alarm interrupt on DS3231 for that time
+
+  }
+  
 }
 
 void bluetoothMode(){
-  if(state == LOW){
+  if(digitalRead(STATE_PIN) == LOW){
     pair();
   }
   BTserial.write("\n--Bluetooth Mode Active--");
@@ -229,9 +267,42 @@ void scheduleMode(){
 
 
     // load schedule from EEPROM
-    // load();
+    load(true);
+    delay(6000);
 
+    // enter low power mode
+    // set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   }
+}
+
+String nextEvent(String events[], int numEvents) {
+  DateTime now = rtc.now();      // get current date/time from RTC module
+  int dayOfWeek = now.dayOfTheWeek();
+  int currentHour = now.hour();
+  int currentMinute = now.minute();
+
+  DateTime closestEventTime = 0;
+  String closestEvent = "";
+
+  for (int i = 0; i < numEvents; i++) {
+    String event = events[i];
+    int eventDOW = event.substring(0,1).toInt();
+    int eventHour = event.substring(2,4).toInt();
+    int eventMinute = event.substring(5,7).toInt();
+
+    time_t eventTime = nextMidnight(currentTime) + (eventDOW - currentDOW) * SECS_PER_DAY + eventHour * SECS_PER_HOUR + eventMinute * SECS_PER_MIN;
+    if (eventDOW < currentDOW || (eventDOW == currentDOW && (eventHour < currentHour || (eventHour == currentHour && eventMinute <= currentMinute)))) {
+      // The event is in the past or happening now
+      // Add one week to the time
+      eventTime += SECS_PER_WEEK;
+    }
+
+    if (closestEventTime == 0 || eventTime < closestEventTime) {
+      closestEventTime = eventTime;
+      closestEvent = events[i];
+    }
+  }
+  return closestEvent;
 }
 
 void dev(){
@@ -298,7 +369,29 @@ void servo(int state){
   }
 }
 
+void set_rtc(){
+  BTserial.write("--Set Time & Day (d:hh:mm)--");
+  while(true){
+    if (BTserial.available()) {
+      String time = BTserial.readString();
+      Serial.println(time);
+      int day = time.substring(0, 1).toInt();
+      int hour = time.substring(2, 4).toInt();
+      int minute = time.substring(5, 7).toInt();
+      break;
+    }
+  }
+}
+
 void button_interrupt(){
   // exit to main menu
   Serial.println("Button Pressed");
+}
+
+void rtcInterrupt(){
+  if (rtc.alarmFired(1)) { 
+      Serial.println("Alarm!");
+      delay(1000); // wait for a second
+      rtc.clearAlarm(1); // clear alarm flag 
+    }
 }
